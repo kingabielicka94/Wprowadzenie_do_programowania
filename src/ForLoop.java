46396f84d86adf8fd7c9eae486a9470d01632449
Kinga Bielicka <kinga.orzeszke@gmail.com>
2022-07-23 12:53:37 +0200

    nauka incrementacji

---
+++ src/ForLoop.java
@@ -0,0 +1,19 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y = 3;
+
+        System.out.println(x++);
+        System.out.println(++x);
+        y = x++;
+
+        System.out.println(y);
+        y= ++x;
+
+        System.out.println(y);
+        System.out.println(++y);
+
+    }
+}
