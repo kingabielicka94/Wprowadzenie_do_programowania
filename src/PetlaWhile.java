56caeb8f4b2abb6072a8fa5069fd16fb6a2fafae
Kinga Bielicka <kinga.orzeszke@gmail.com>
2022-07-23 16:02:04 +0200

    pętla while i pętla while do

---
+++ src/PetlaWhile.java
@@ -0,0 +1,16 @@
+public class PetlaWhile {
+
+    public static void main(String[] args) {
+        int licznik = 0;
+//        while(licznik < 10){
+//            licznik++;
+//            System.out.println(licznik);
+//        }
+
+        do {
+            licznik++;
+            System.out.println(licznik);
+        } while (licznik < 10);
+    }
+}
+//while najpierw sprawdza warunek, a do while najpierw wykonuje instrukcje, a potem sprawdza warunek
