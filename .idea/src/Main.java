06191166844fc57f2225660fcb8561544ecc210e
Kinga Bielicka <kinga.orzeszke@gmail.com>
2022-07-23 11:26:33 +0200

    Dodałam zmienną o nazwie age

--- .idea/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        String name = "Kinga";
-        int age = 24;
-        System.out.println("Hello "+ name +"! Mam" + age + "lat.");
-        char a = 'b';
-
-    }
-}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 28;
+        System.out.println("Hello Kinga" + "Mam" + age);
+    }
+}
