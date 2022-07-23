a67558b6cb4f067afecfb6174f225ecc15f9b873
Kinga Bielicka <kinga.orzeszke@gmail.com>
2022-07-23 13:30:14 +0200

    pokazanie tablicy kotków

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,14 +1,14 @@
 public class ForLoop {
-
+    //komentarz
     public static void main(String[] args) {
-        //komentarz
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
-            if (i==5) {
-                System.out.println("kotek");
-            }
+
+        String[] catBoard = new String[]{"Karmi", "Bubu", "Łapki", "Simba", "Tofik"};
+
+        for (int i = 0; i < catBoard.length; i++) {
+            System.out.println(catBoard[i]);
         }
-
-
+        for (String cat: catBoard)
+            System.out.println(cat);
     }
+
 }
